c860195962cde2af0ad19d0cfa7efc03c86e7e79
Rajat Jain <jain.rajat50@gmail.com>
2018-12-21 16:55:22 +0800

    Added Random Numbers CPP code in the script

--- random-numbers.sh
+++ random-numbers.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+cppfile="#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+using namespace std;
+int main(){
+    srand (time(NULL));
+    int a,b,c,threshold;
+    cin >> a >> b >> c;
+    for (int i = 0 ; i < c ; i++){
+        int value = rand() % abs(b-a+1) + min(a,b);
+        cout << value << endl;
+   }
+    return 0;
+}"
+cat $cppfile > random.cpp
 g++ random.cpp -o random.o
 ./random.o < input.txt > output.txt
 output="`cat output.txt`"
